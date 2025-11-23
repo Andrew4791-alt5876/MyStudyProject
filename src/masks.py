13d3f1b0805857b639d8ecfa064c61759d17ff18
Andrei Tiurin <tyrandr@list.ru>
2025-11-23 22:34:09 +0300

    correct src fixes

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,6 @@
 def get_mask_card_number(user_card_number: str) -> str:
     """Функция, которая принимает на вход номер карты в виде числа и возвращает маску"""
-    mask_card_number = (f"{user_card_number[:4]} "
-                        f"{user_card_number[4:6]}** **** {user_card_number[-4:]}")
+    mask_card_number = f"{user_card_number[:4]} " f"{user_card_number[4:6]}** **** {user_card_number[-4:]}"
     return mask_card_number
 
 
--- src/widget.py
+++ src/widget.py
@@ -1,15 +1,12 @@
-import masks
+from src import masks
 
 
 def mask_account_card(account_number_card: str) -> str:
     """Функция, которая принимает тип и номер карты или счета и возвращает данные с маской"""
     if account_number_card[-20:].isdigit():
-        mask_account_card = masks.get_mask_account(account_number_card)
+        mask_account_card = f"{account_number_card[:5]}" f"{masks.get_mask_account(account_number_card)}"
     else:
-        #card_number = account_number_card[-16:]
-        #name_card = account_number_card[:-16]
-        #card_number_mask = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
-        mask_account_card = masks.get_mask_card_number(account_number_card)
+        mask_account_card = f"{account_number_card[:-16]}" f"{masks.get_mask_card_number(account_number_card[-16:])}"
     return mask_account_card
 
 
@@ -17,6 +14,3 @@ def get_date(date_time: str) -> str:
     """Функция, которая возвращает дату"""
     date_final_str = f"{date_time[8:10]}.{date_time[5:7]}.{date_time[:4]}"
     return date_final_str
-
-account_number_card = "Счет 64686473678894779589"
-print(mask_account_card(account_number_card))
