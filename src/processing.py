6c444b0d7adf682cc1a73296be25d79711e3c6e8
Andrei Tiurin <tyrandr@list.ru>
2025-11-25 20:04:38 +0300

    processing final fixes

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 
-from src import widget
 
-
-def filter_by_state(info_about_users: list, state: str = "EXECUTED") -> list:
-    pass
+def filter_by_state(users_info_state: list, state: str) -> list:
+    filter_users_state = []
+    for user_info_state in users_info_state:
+        if user_info_state["state"] == "EXECUTED" and state != "CANCELED":
+            filter_users_state.append(user_info_state)
+        elif user_info_state["state"] == "CANCELED" and state == "CANCELED":
+            filter_users_state.append(user_info_state)
+    return filter_users_state
 
 
 def sort_by_date(info_about_users: list) -> list:
@@ -14,22 +18,21 @@ def sort_by_date(info_about_users: list) -> list:
     )
     return sort_by_date
 
-    # info_users_date = [item.copy() for item in info_about_users]
-    # for info_date in info_users_date:
-    #     info_only_date = widget.get_date(info_date["date"])
-    #     info_date["date"] = info_only_date
-    # new_users_date_sort = sorted(info_users_date, reverse=True, key=lambda x: datetime.strptime(x["date"], "%d.%m.%Y"))
-    # for info_about_user in info_about_users:
-    #     for new_user_date_sort in new_users_date_sort:
-    #         if widget.get_date(info_about_user["date"]) == new_user_date_sort["date"]:
-    #             new_user_date_sort["date"] = info_about_user["date"]
-    # return new_users_date_sort
 
-
-info_about_users = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-print(sort_by_date(info_about_users))
+# state = "CANCELED"
+# users_info_state = [
+#     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+#     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+#     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+#     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+# ]
+# print(filter_by_state(users_info_state, state))
+#
+#
+# info_about_users = [
+#     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+#     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+#     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+#     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+# ]
+# print(sort_by_date(info_about_users), state)
