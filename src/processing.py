3fa569b7722054fcb544a14e1eaecd1beeb7e231
Andrei Tiurin <tyrandr@list.ru>
2025-11-27 23:35:50 +0300

    final fixes myproject

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 
-def filter_by_state(users_info_state: list, state: str) -> list:
+def filter_by_state(users_info_state: list, state: str = "EXECUTED") -> list:
     """Функция, которая возвращает новый список словарей, содержащий только те словари, у которых ключ
     state соответствует указанному значению"""
     filter_users_state = []
@@ -21,20 +21,20 @@ def sort_by_date(info_about_users: list) -> list:
     return sort_by_date
 
 
-# state = "CANCELED"
-# users_info_state = [
-#     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-#     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-#     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-#     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-# ]
-# print(filter_by_state(users_info_state, state))
-#
-#
-# info_about_users = [
-#     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-#     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-#     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-#     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-# ]
-# print(sort_by_date(info_about_users), state)
+state = "CANCELED"
+users_info_state = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+print(filter_by_state(users_info_state, state))
+
+
+info_about_users = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+print(sort_by_date(info_about_users), state)
