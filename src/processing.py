839b01c7c0930872cb1bdc028e42c97ecef7352d
Andrei Tiurin <tyrandr@list.ru>
2025-12-01 21:52:16 +0300

    fixes for feature filter by state

--- src/processing.py
+++ src/processing.py
@@ -13,15 +13,7 @@ def filter_by_state(users_info_state: list, state: str = "EXECUTED") -> list:
     return filter_users_state
 
 
-def sort_by_date(info_about_users: list) -> list:
-    """Функция, которая сортирует входные данные по дате"""
-    sort_by_date = sorted(
-        info_about_users, reverse=True, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f")
-    )
-    return sort_by_date
-
-
-state = ""
+state = "CANCELED"
 users_info_state = [
     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -29,12 +21,3 @@ users_info_state = [
     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 print(filter_by_state(users_info_state, state))
-
-
-info_about_users = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-print(sort_by_date(info_about_users), state)
