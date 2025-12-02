96d44fd242e02684c0b95c41588386a85ceff05e
Andrei Tiurin <tyrandr@list.ru>
2025-12-02 23:32:15 +0300

    test masks fixes

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,24 @@
 def get_mask_card_number(user_card_number: str) -> str:
     """Функция, которая принимает на вход номер карты в виде числа и возвращает маску"""
-    mask_card_number = f"{user_card_number[:4]} " f"{user_card_number[4:6]}** **** {user_card_number[-4:]}"
-    return mask_card_number
+    if isinstance(user_card_number, str):
+        numbers = user_card_number.replace(" ", "")
+        if len(numbers) == 16 and numbers.isdigit():
+            mask_card_number = f"{numbers[:4]} " f"{numbers[4:6]}** **** {numbers[-4:]}"
+            return mask_card_number
+        else:
+            return "Ошибка ввода номера карты!"
+    else:
+        return "Ошибка ввода номера карты!"
 
 
 def get_mask_account(user_account: str) -> str:
     """Функция, которая принимает на вход номер счета в виде числа и возвращает маску"""
-    mask_account = f"**{user_account[-4:]}"
-    return mask_account
+    if isinstance(user_account, str):
+        numbers_of_account = user_account.replace(" ", "")
+        if len(numbers_of_account) == 20 and numbers_of_account.isdigit():
+            mask_account = "**" + numbers_of_account[-4:]
+            return mask_account
+        else:
+            return "Ошибка ввода номера счета!"
+    else:
+        return "Ошибка ввода номера счета!"
---
+++ tests/test_masks.py
@@ -0,0 +1,28 @@
+import pytest
+
+from src.masks import get_mask_card_number, get_mask_account
+
+
+@pytest.mark.parametrize('card_number, mask_card_number', [
+    ('7000792289606361', '7000 79** **** 6361'),
+    ('2200 5436 5456 9080', '2200 54** **** 9080'),
+    (1234234567894563, 'Ошибка ввода номера карты!'),
+    ('123456789012345', 'Ошибка ввода номера карты!'),
+    ('number3456 4556 65', 'Ошибка ввода номера карты!'),
+    ('', 'Ошибка ввода номера карты!'),
+    ((1, 2, 3), 'Ошибка ввода номера карты!')
+])
+def test_get_mask_card_number(card_number, mask_card_number):
+    assert get_mask_card_number(card_number) == mask_card_number
+
+@pytest.mark.parametrize('account, mask_account', [
+    ('73654108430135874305', '**4305'),
+    ('12345 12345 12345 12345', '**2345'),
+    (12345123451234512345, 'Ошибка ввода номера счета!'),
+    ('1234565432345676543', 'Ошибка ввода номера счета!'),
+    ('account1234 51231 2345', 'Ошибка ввода номера счета!'),
+    ('', 'Ошибка ввода номера счета!'),
+    ((1, 2, 3), 'Ошибка ввода номера счета!')
+])
+def test_get_mask_account(account, mask_account):
+    assert get_mask_account(account) == mask_account
