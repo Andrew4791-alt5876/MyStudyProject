8d19cbf1a6dbc710a284cd78210eacb775476bf8
Andrei Tiurin <tyrandr@list.ru>
2025-12-02 18:34:29 +0300

    fixes_10_1

--- src/processing.py
+++ src/processing.py
@@ -19,22 +19,3 @@ def sort_by_date(info_about_users: list) -> list:
         info_about_users, reverse=True, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f")
     )
     return sort_by_date
-
-
-state = "vhjvjb"
-users_info_state = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-print(filter_by_state(users_info_state, state))
-
-
-info_about_users = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-print(sort_by_date(info_about_users), state)
