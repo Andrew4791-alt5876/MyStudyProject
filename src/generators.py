ba9ecb26d1c1a783dbcf0eee4d2872f1819b3698
Andrei Tiurin <tyrandr@list.ru>
2025-12-03 22:12:32 +0300

    generators fixes

--- src/generators.py
+++ src/generators.py
@@ -14,11 +14,11 @@ def transaction_descriptions(transactions):
 
 
 
-usd_transactions = filter_by_currency(transactions, "USD")
-for _ in range(2):
-    print(next(usd_transactions))
-
-
-descriptions = transaction_descriptions(transactions)
-for _ in range(5):
-    print(next(descriptions))
+# usd_transactions = filter_by_currency(transactions, "USD")
+# for _ in range(2):
+#     print(next(usd_transactions))
+#
+#
+# descriptions = transaction_descriptions(transactions)
+# for _ in range(5):
+#     print(next(descriptions))
