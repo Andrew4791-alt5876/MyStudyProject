c84406880df132b4ffc14870d2db428ff15a52e0
Andrei Tiurin <tyrandr@list.ru>
2025-12-03 03:06:33 +0300

    test processing fixes

---
+++ tests/test_processing.py
@@ -0,0 +1,21 @@
+from src.processing import filter_by_state, sort_by_date
+
+
+def test_filter_by_state_canceled(info_users_state, filter_users_state_canceled):
+    assert filter_by_state(info_users_state, state='CANCELED') == filter_users_state_canceled
+
+
+def test_filter_by_state_executed(info_users_state, filter_users_state_executed):
+    assert filter_by_state(info_users_state, state='EXECUTED') == filter_users_state_executed
+
+
+def test_filter_by_state_executed_empty(info_users_state, filter_users_state_executed):
+    assert filter_by_state(info_users_state, state='') == filter_users_state_executed
+
+
+def test_filter_by_state_executed_wrong(info_users_state, filter_users_state_executed):
+    assert filter_by_state(info_users_state, state='hfwhvfWH') == filter_users_state_executed
+
+
+def test_sort_by_date(sort_by_date_enter, sort_by_date_exit):
+    assert sort_by_date(sort_by_date_enter) == sort_by_date_exit
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -3,22 +3,30 @@ import pytest
 from src.widget import mask_account_card_fun, get_date
 
 
-@pytest.mark.parametrize('account_number_card, mask_account_card', [
-    ('Maestro 1596837868705199', 'Maestro 1596 83** **** 5199'),
-    ('Счет 64686473678894779589', 'Счет **9589'),
-    ('Счет fghsgjhjhs4534567655', 'Не верный номер счета или карты!'),
-    ('Visa Platinum 8990922113665229', 'Visa Platinum 8990 92** **** 5229'),
-    (12341234134344555464, 'Не верный номер счета или карты!'),
-    ('Visa Classic 68319824767376567', 'Не верный номер счета или карты!'),
-    ('Счет 7365410843013587430', 'Не верный номер счета или карты!'),
-    ('Visa Gold 599941422842635', 'Не верный номер счета или карты!')])
+@pytest.mark.parametrize(
+    "account_number_card, mask_account_card",
+    [
+        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("Счет 64686473678894779589", "Счет **9589"),
+        ("Счет fghsgjhjhs4534567655", "Не верный номер счета или карты!"),
+        ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
+        (12341234134344555464, "Не верный номер счета или карты!"),
+        ("Visa Classic 68319824767376567", "Не верный номер счета или карты!"),
+        ("Счет 7365410843013587430", "Не верный номер счета или карты!"),
+        ("Visa Gold 599941422842635", "Не верный номер счета или карты!"),
+    ],
+)
 def test_mask_account_card(account_number_card, mask_account_card):
     assert mask_account_card_fun(account_number_card) == mask_account_card
 
 
-@pytest.mark.parametrize('date_time, get_date_str', [
-    ('2024-03-11T02:26:18.671407', '11.03.2024'),
-    ('03-11T02:26:18.671407', 'Не верный формат даты!'),
-    (19800405687787977, 'Не верный формат даты!')])
+@pytest.mark.parametrize(
+    "date_time, get_date_str",
+    [
+        ("2024-03-11T02:26:18.671407", "11.03.2024"),
+        ("03-11T02:26:18.671407", "Не верный формат даты!"),
+        (19800405687787977, "Не верный формат даты!"),
+    ],
+)
 def test_get_date(date_time, get_date_str):
     assert get_date(date_time) == get_date_str
