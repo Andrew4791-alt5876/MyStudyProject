912b4519b5f2d8caa36c6ec2beea22a059ab0361
Andrei Tiurin <tyrandr@list.ru>
2025-12-06 01:23:43 +0300

    test filter by currency fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -118,7 +118,9 @@ def info_transactions_usd():
             "description": "Перевод со счета на счет",
             "from": "Счет 19708645243227258542",
             "to": "Счет 75651667383060284188",
-        }, [], {
+        },
+        [],
+        {
             "id": 895315941,
             "state": "EXECUTED",
             "date": "2018-08-19T04:27:37.904916",
@@ -126,7 +128,35 @@ def info_transactions_usd():
             "description": "Перевод с карты на карту",
             "from": "Visa Classic 6831982476737658",
             "to": "Visa Platinum 8990922113665229",
-        }, []
+        },
+        [],
+    ]
+
+
+@pytest.fixture
+def info_transactions_rub():
+    return [
+        [],
+        [],
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160",
+        },
+        [],
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657",
+        },
     ]
 
 
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,19 +1,26 @@
+from typing import Generator, Any
 from src.generators import filter_by_currency
 
 
 def test_filter_by_currency_usd(info_transactions, info_transactions_usd):
-    test_generators = filter_by_currency(info_transactions, "USD")
+    test_generator = filter_by_currency(info_transactions, "USD")
     for i in range(5):
-        assert next(test_generators) == info_transactions_usd[i]
+        assert next(test_generator) == info_transactions_usd[i]
+
+
+def test_filter_by_currency_rub(info_transactions, info_transactions_rub):
+    test_generator = filter_by_currency(info_transactions, "RUB")
+    for i in range(5):
+        assert next(test_generator) == info_transactions_rub[i]
 
 
 def test_filter_by_currency_not_money(info_transactions, info_transactions_empty):
-    test_generators = filter_by_currency(info_transactions, "")
+    test_generator = filter_by_currency(info_transactions, "")
     for i in range(5):
-        assert next(test_generators) == info_transactions_empty
+        assert next(test_generator) == []
 
 
 def test_filter_by_currency_empty_list(info_transactions_empty_list, info_transactions_empty):
     test_generators = filter_by_currency(info_transactions_empty_list, "USD")
     for i in range(5):
-        assert list(test_generators) == info_transactions_empty
+        assert list(test_generators) == []
