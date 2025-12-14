a3ac7cdb50aea92d326460df911ce3757baa4b84
Andrei Tiurin <tyrandr@list.ru>
2025-12-14 10:27:46 +0300

    decorators fixes new

--- src/decorators.py
+++ src/decorators.py
@@ -1,9 +1,13 @@
 from time import time
+from typing import Any
 
 
-def log(filename=None):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
+def log(filename: Any | None = None) -> Any:
+    """Внешняя функция, которая принимает аргумент для декоратора и возвращает внутренний декоратор decorator."""
+    def decorator(func: Any) -> Any:
+        """Декоратор, который автоматически регистрирует детали выполнения функций. Возвращает wrapper."""
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            """Функция-обертка wrapper, которая определяет детали выполнения функции."""
             try:
                 start_time = time()
                 resalt = func(*args, **kwargs)
@@ -11,16 +15,15 @@ def log(filename=None):
                 time_of_work = end_of_time - start_time
                 resalt_log = (
                     f"Функция {func.__name__} is OK\n"
-                    f"Начало работы функции {start_time}\n"
-                    f"Конец работы функции {end_of_time}\n"
-                    f"Время работы функции {time_of_work}\n"
-                    f"Результат функции {resalt}"
+                    f"Время работы функции: {time_of_work}\n"
+                    f"Передаваемые аргументы: {args}, {kwargs}\n"
+                    f"Результат функции: {resalt}"
                 )
                 if filename == "mylog.txt":
                     with open(filename, "w", encoding="utf-8") as file:
                         file.write(resalt_log)
                 else:
-                    return resalt_log
+                    print(resalt_log)
             except Exception as e:
                 error_type = type(e).__name__
                 log_message = f"Функция {func.__name__} error: {error_type}. Inputs: {args}, {kwargs}"
@@ -28,14 +31,15 @@ def log(filename=None):
                     with open(filename, "w", encoding="utf-8") as file:
                         file.write(log_message)
                 else:
-                    return log_message
+                    print(log_message)
         return wrapper
     return decorator
 
 
 @log(filename="mylog.txt")
-def my_function(x, y):
+def my_function(x: Any, y: Any) -> Any:
+    '''Функция для декорирования.'''
     return x / y
 
 
-print(my_function(5, 2))
+my_function(20, 3)
