73a9cbbabe59c8005b3c79543fdd8313267ab18f
Andrei Tiurin <tyrandr@list.ru>
2025-12-17 07:11:46 +0300

    impruv stat fixes

--- src/generators.py
+++ src/generators.py
@@ -30,10 +30,18 @@ def transaction_descriptions(transactions: list) -> Generator[Any, Any, None]:
 
 def card_number_generator(start_number: int, finish_number: int) -> Generator[str, Any, None]:
     """Генератор, который выдает номера банковских карт в заданном диапазоне."""
-    for number in range(start_number, finish_number + 1):
-        zero_str = "".join(["0" for z in range(16 - len(str(number))) if len(str(number)) <= 16])
-        card_number_str = zero_str + str(number)
-        card_number_exit = (
-            f"{card_number_str[:4]} {card_number_str[4:8]} {card_number_str[8:12]} {card_number_str[12:]}"
-        )
-        yield card_number_exit
+    try:
+        if isinstance(start_number, int) and isinstance(finish_number, int) and start_number >= 0 and finish_number >= 0:
+            for number in range(start_number, finish_number + 1):
+                zero_str = "".join(["0" for z in range(16 - len(str(number))) if len(str(number)) <= 16])
+                card_number_str = zero_str + str(number)
+                card_number_exit = (f"{card_number_str[:4]} {card_number_str[4:8]} "
+                                f"{card_number_str[8:12]} {card_number_str[12:]}")
+                yield card_number_exit
+        else:
+            yield 'Не верные данные.'
+    except Exception:
+        pass
+
+
+
