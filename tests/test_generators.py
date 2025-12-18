4102bf0450b4de4743467ccfb19df02fe5038628
Andrei Tiurin <tyrandr@list.ru>
2025-12-18 08:03:43 +0300

    impruv stat fixes

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -58,7 +58,18 @@ def test_card_number_generator_big(card_number_generator_exit_999: list) -> None
 
 
 def test_card_number_generator_no_data() -> None:
-    """Тест генератора номера карты в диапазоне от 9000800070006000 до 9000800070006020."""
+    """Тест генератора номера карты при отсутствии данных ввода."""
     generator_card = card_number_generator()
-    for i in range(5):
-        assert next(generator_card) == None
+    assert next(generator_card) == "Не верные данные."
+
+
+def test_card_number_generator_not_right_data() -> None:
+    """Тест генератора номера карты при не верном вводе данных."""
+    generator_card = card_number_generator(567, 7)
+    assert next(generator_card) == "Не верные данные."
+
+
+def test_card_number_generator_not_typeerror() -> None:
+    """Тест генератора номера карты при не верном вводе данных."""
+    generator_card = card_number_generator('567', 7)
+    assert next(generator_card) == "Не верные данные."
