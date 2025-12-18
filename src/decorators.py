dd200a8f90d9f3b4b1585500348cbec29d2a8e23
Andrei Tiurin <tyrandr@list.ru>
2025-12-18 20:59:36 +0300

    final 11_2 fixes

--- src/decorators.py
+++ src/decorators.py
@@ -1,4 +1,4 @@
-from time import time
+import time
 from typing import Any, Callable
 
 
@@ -11,12 +11,13 @@ def log(filename: Any | None = None) -> Any:
         def wrapper(*args: Any, **kwargs: Any) -> Any:
             """Функция-обертка, которая логирует детали выполнения функции."""
             try:
-                start_time = time()
+                start_time = time.strftime("%a %d %b %Y %H:%M:%S")
+                start_time_sec = time.time()
                 resalt = func(*args, **kwargs)
-                end_time = time()
-                time_of_work = end_time - start_time
-                resalt_log = f"Функция {func.__name__} is OK\n" f"Время выполнения функции: {time_of_work}"
-                if filename == "mylog.txt":
+                end_time = time.time()
+                time_of_work = end_time - start_time_sec
+                resalt_log = f"[{start_time}]\nФункция {func.__name__} OK\n Время выполнения функции: {time_of_work}"
+                if filename:
                     with open(filename, "a", encoding="utf-8") as file:
                         file.write(resalt_log + "\n")
                 else:
@@ -27,7 +28,7 @@ def log(filename: Any | None = None) -> Any:
                 log_message = (
                     f"Функция {func.__name__}, " f"Ошибка: {error_type}, " f"Входные аргументы: {args}, {kwargs}"
                 )
-                if filename == "mylog.txt":
+                if filename:
                     with open(filename, "a", encoding="utf-8") as file:
                         file.write(log_message + "\n")
                     return log_message
