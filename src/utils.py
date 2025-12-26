60230368d02c299f9ee99a4e5993cb0171d59db0
Andrei Tiurin <tyrandr@list.ru>
2025-12-26 20:08:50 +0300

    all functions ready homework 12_1

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,17 @@
 import json
+from typing import Any
 
 
-def read_json_file(path: str) -> list:
+def read_json_file(path: str) -> list[Any]:
     """Функция, которая считывает и преобразует JSON-файл в Python-список"""
     try:
         with open(path, "r", encoding="utf-8") as file:
             try:
                 data_operations = json.load(file)
-                return data_operations
+                if isinstance(data_operations, list):
+                    return data_operations
+                else:
+                    return []
             except json.JSONDecodeError:
                 return []
     except FileNotFoundError:
