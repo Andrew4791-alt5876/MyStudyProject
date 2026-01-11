1f3461736d9d2377171d8a180b0a9cda2321f205
Andrei Tiurin <tyrandr@list.ru>
2026-01-12 00:09:11 +0300

    fixes homework correct 12_2

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,9 @@
 import logging
 
-
-logger = logging.getLogger('masks')
+logger = logging.getLogger("masks")
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler('../logs/masks.log', encoding='utf-8')
-file_formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler = logging.FileHandler("../logs/masks.log", "w", encoding="utf-8")
+file_formater = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formater)
 logger.addHandler(file_handler)
 
@@ -14,14 +13,14 @@ def get_mask_card_number(user_card_number: str) -> str:
     if isinstance(user_card_number, str):
         numbers = user_card_number.replace(" ", "")
         if len(numbers) == 16 and numbers.isdigit():
-            logger.info(f'Маска номера карты клиента создана')
+            logger.info("Маска номера карты клиента создана")
             mask_card_number = f"{numbers[:4]} " f"{numbers[4:6]}** **** {numbers[-4:]}"
             return mask_card_number
         else:
-            logger.error(f'Ошибка ввода номера карты')
+            logger.error("Ошибка ввода номера карты")
             return "Ошибка ввода номера карты!"
     else:
-        logger.error(f'Ошибка ввода номера карты')
+        logger.error("Ошибка ввода номера карты")
         return "Ошибка ввода номера карты!"
 
 
@@ -30,16 +29,16 @@ def get_mask_account(user_account: str) -> str:
     if isinstance(user_account, str):
         numbers_of_account = user_account.replace(" ", "")
         if len(numbers_of_account) == 20 and numbers_of_account.isdigit():
-            logger.info(f'Маска номера счета клиента создана')
+            logger.info("Маска номера счета клиента создана")
             mask_account = "**" + numbers_of_account[-4:]
             return mask_account
         else:
-            logger.error(f'Ошибка ввода номера счета')
+            logger.error("Ошибка ввода номера счета")
             return "Ошибка ввода номера счета!"
     else:
-        logger.error(f'Ошибка ввода номера счета')
+        logger.error("Ошибка ввода номера счета")
         return "Ошибка ввода номера счета!"
 
 
-print(get_mask_card_number('2234234523457891'))
-print(get_mask_account('12345123451234512341'))
+print(get_mask_card_number("223423452345789"))
+print(get_mask_account("12345123451234512341"))
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,10 @@
 import json
 import logging
 
-
-logger = logging.getLogger('utils')
+logger = logging.getLogger("utils")
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler('../logs/utils.log', encoding='utf-8')
-file_formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler = logging.FileHandler("../logs/utils.log", "w", encoding="utf-8")
+file_formater = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formater)
 logger.addHandler(file_handler)
 
@@ -13,23 +12,23 @@ logger.addHandler(file_handler)
 def read_json_file(path: str = "") -> list:
     """Функция, которая считывает и преобразует JSON-файл в Python-список"""
     if not isinstance(path, str):
-        logger.error('Не верный путь к JSON-файлу')
+        logger.error("Не верный путь к JSON-файлу")
         return []
     try:
         with open(path, "r", encoding="utf-8") as file:
             try:
                 data_operations = json.load(file)
-                logger.info('JSON-файл в Python-список преобразован успешно')
+                logger.info("JSON-файл в Python-список преобразован успешно")
                 if isinstance(data_operations, list):
                     return data_operations
                 else:
                     return []
             except json.JSONDecodeError:
-                logger.error('Не верный формат JSON-файла')
+                logger.error("Не верный формат JSON-файла")
                 return []
     except (FileNotFoundError, PermissionError, SyntaxError, TypeError, OSError):
-        logger.error('Ошибка при считывании JSON-файла')
+        logger.error("Ошибка при считывании JSON-файла")
         return []
 
 
-print(read_json_file('/data/operations.json'))
+print(read_json_file("/data/operations.json"))
