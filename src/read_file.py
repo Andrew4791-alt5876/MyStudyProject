3a3f2438e986ffbce691450028f0bbb78d0b0d9f
Andrei Tiurin <tyrandr@list.ru>
2026-01-24 21:03:23 +0300

    func fixes new homework 13.1

--- src/read_file.py
+++ src/read_file.py
@@ -1,15 +1,31 @@
 import pandas as pd
 
 
-def read_csv_file(file_path_csv: str) -> list[dict]:
+def read_csv_file(file_path_csv: str = '') -> list[dict]:
     """Функция, которая преобразует csv-файл в python базу данных"""
-    df_csv_file = pd.read_csv(file_path_csv, sep=";")
-    list_df_csv_file = df_csv_file.to_dict("records")
-    return list_df_csv_file
+    if not isinstance(file_path_csv, str):
+        return []
+    try:
+        df_csv_file = pd.read_csv(file_path_csv, sep=";")
+        list_df_csv_file = df_csv_file.to_dict("records")
+        if isinstance(list_df_csv_file, list):
+            return list_df_csv_file
+        else:
+            return []
+    except (FileNotFoundError, PermissionError, SyntaxError, TypeError, OSError):
+        return []
 
 
-def read_excel_file(file_path_excel: str) -> list[dict]:
+def read_excel_file(file_path_excel: str = '') -> list[dict]:
     """Функция, которая преобразует excel-файл в python базу данных"""
-    df_excel_file = pd.read_excel(file_path_excel)
-    list_df_excel_file = df_excel_file.to_dict("records")
-    return list_df_excel_file
+    if not isinstance(file_path_excel, str):
+        return []
+    try:
+        df_excel_file = pd.read_excel(file_path_excel)
+        list_df_excel_file = df_excel_file.to_dict("records")
+        if isinstance(list_df_excel_file, list):
+            return list_df_excel_file
+        else:
+            return []
+    except (FileNotFoundError, PermissionError, SyntaxError, TypeError, OSError):
+        return []
---
+++ tests/test_read_file.py
@@ -0,0 +1,15 @@
+from unittest.mock import mock_open, patch
+
+from src.read_file import read_csv_file, read_excel_file
+
+
+def test_read_csv_file_with_csv_file() -> None:
+    pass
+    """Тест функции преобразования csv-файла при корректном файле и пути до него."""
+
+    # test_data = None
+    # with patch("builtins.open", mock_open(read_data=test_data)):
+    #     resalt = read_csv_file("../data/transactions.csv")
+    # assert resalt == None
+
+
